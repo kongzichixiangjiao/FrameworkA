9cc75c5b5356daaa328032ec48cdc1d97ca3fc61
侯佳男 <houjianan@houjianandeiMac.local>
2019-01-14 10:10:59 +0800

    Initial commit

---
+++ Classes/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  FrameworkA
+//
+//  Created by 侯佳男 on 2019/1/14.
+//  Copyright © 2019年 侯佳男. All rights reserved.
+//
+
+import Foundation
+
+open class Test {
+    public func test() {
+        print("test")
+    }
+}
---
+++ FrameworkA.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkA.h
+//  FrameworkA
+//
+//  Created by 侯佳男 on 2019/1/14.
+//  Copyright © 2019年 侯佳男. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkA.
+FOUNDATION_EXPORT double FrameworkAVersionNumber;
+
+//! Project version string for FrameworkA.
+FOUNDATION_EXPORT const unsigned char FrameworkAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkA/PublicHeader.h>
+
+
